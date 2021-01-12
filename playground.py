89621f1f7a0ee973bc41acadd605e99e0b1a14c1
Stasqq <stachocz@gmail.com>
2021-01-12 18:12:19 +0100

    czyszczenie listy produktów

--- playground.py
+++ playground.py
@@ -1,6 +1,12 @@
-def for_loop(index):
-    for i in range(index, -1, -1):
-        print(i)
+from json_handler.json_handler import load_jsonl_data
+from preprocessing.junk_remover import JunkRemover
 
+sessions = load_jsonl_data("data/input/sessions.jsonl")
+products = load_jsonl_data("data/input/products.jsonl")
+jr = JunkRemover()
 
-for_loop(10)
+clean_sessions, clean_products = jr.delete_junk_data(sessions, products)
+
+for i in range(0, len(clean_products)):
+    print(clean_products[i])
+
--- preprocessing/junk_remover.py
+++ preprocessing/junk_remover.py
@@ -1,15 +1,21 @@
 from preprocessing.session_user_filler import SessionUserFiller
 from preprocessing.timestamp_handler import TimestampHandler
 
+MIN_PRODUCT_PRICE = 1
+MAX_PRODUCT_PRICE = 10000
 
-class FunkRemover:
+class JunkRemover:
 
     def __init__(self):
         self._session_user_filler = SessionUserFiller()
         self._timestamp_handler = TimestampHandler()
 
     def delete_junk_data(self, sessions, products):
-        new_sessions = self._clear_sessions(sessions)
+        pre_sessions = self._clear_sessions(sessions)
+        new_products = self._clear_products(products)
+        new_sessions = self._remove_sessions_with_removed_products(pre_sessions, new_products)
+
+        return new_sessions, new_products
 
     def _clear_sessions(self, sessions):
         sessions = self._session_user_filler.fill_sessions_users(sessions)
@@ -25,3 +31,12 @@ class FunkRemover:
     def _drop_buy_sessions_without_purchase_id(self, sessions):
         return [session for session in sessions if not
         (session['event_type'] == 'BUY_PRODUCT' and session['purchase_id'] is None)]
+
+    def _clear_products(self, products):
+        return [product for product in products if MIN_PRODUCT_PRICE <= product['price'] <= MAX_PRODUCT_PRICE]
+
+    def _remove_sessions_with_removed_products(self, pre_sessions, new_products):
+        new_product_ids = set()
+        for product in new_products:
+            new_product_ids.add(product['product_id'])
+        return [session for session in pre_sessions if session['product_id'] in new_product_ids]
